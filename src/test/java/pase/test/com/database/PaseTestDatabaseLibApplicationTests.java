03b915c6e8efa2e23adb85e17f0c2f64dcd3618d
ErickMontalvo732 <eriick732@gmail.com>
2025-07-14 21:49:39 -0600

    [PC-00] chore create project structure database lib

---
+++ src/test/java/pase/test/com/database/PaseTestDatabaseLibApplicationTests.java
@@ -0,0 +1,13 @@
+package pase.test.com.database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaseTestDatabaseLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
